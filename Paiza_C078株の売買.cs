6c6ff0f40af58f3a54c98ba1275feef092bfdcb7
calvin8171 <102227342+calvin8171@users.noreply.github.com>
2023-02-13 12:00:12 +0900

    Add files via upload

---
+++ Paiza_C078株の売買.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        String line = Console.ReadLine();
+        String[] paramArr = line.Split(' ');
+
+        int day = int.Parse(paramArr[0]);
+        int buyStockPrice = int.Parse(paramArr[1]);
+        int sellStockPrice = int.Parse(paramArr[2]);
+
+        int stockCnt = 0;
+        int profit = 0;
+
+        for (int i = 0; i < day -1; i++)
+        {
+            line = Console.ReadLine();
+            int todayStockPrice = int.Parse(line);
+
+            if (i == day - 1)
+            {
+                profit += stockCnt > 0 ? stockCnt * todayStockPrice : 0;
+            }
+            else if (todayStockPrice <= buyStockPrice)
+            {
+                profit -= buyStockPrice;
+                stockCnt++;
+            }
+            else if (todayStockPrice >= sellStockPrice)
+            {
+                profit += sellStockPrice * stockCnt;
+                stockCnt = 0;
+            }
+            else if (todayStockPrice > buyStockPrice && todayStockPrice < sellStockPrice)
+            {
+            }
+        }
+        Console.WriteLine(profit);
+    }
+}
